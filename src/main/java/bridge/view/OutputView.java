579dc20c6225c68e19c7d89f5bd7dbe6e5479d51
sosow0212 <sosow0212@naver.com>
2022-12-16 17:15:07 +0900

    feat : (OutputView) 성공한 경우 출력

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,10 +1,14 @@
 package bridge.view;
 
+import bridge.domain.BridgeGame;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
     private final static String ERROR = "[ERROR] ";
+    private StringBuilder up = new StringBuilder();
+    private StringBuilder down = new StringBuilder();
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -12,6 +16,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
+
     }
 
     /**
@@ -20,6 +25,18 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println("\n게임 성공 여부: " + isWin(bridgeGame.isGameWin()));
+        System.out.println("총 시도한 횟수: " + bridgeGame.getTryCount());
+    }
+
+    public String isWin(boolean isWin) {
+        if (isWin) {
+            return "성공";
+        }
+        return "실패";
     }
 
     public void printStart() {
