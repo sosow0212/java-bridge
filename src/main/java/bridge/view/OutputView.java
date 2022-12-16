1db09bde12109ec236c01d01d411a6516a0ab044
sosow0212 <sosow0212@naver.com>
2022-12-16 17:37:18 +0900

    feat : (OutputView) 맵 출력을 위한 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -7,16 +7,55 @@ import bridge.domain.BridgeGame;
  */
 public class OutputView {
     private final static String ERROR = "[ERROR] ";
-    private StringBuilder up = new StringBuilder();
-    private StringBuilder down = new StringBuilder();
+    private static StringBuilder up = new StringBuilder();
+    private static StringBuilder down = new StringBuilder();
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(String direction, boolean isCorrect) {
+        handlePrintMap(direction, isCorrect);
+        System.out.println("[ " + up.toString() + " ]");
+        System.out.println("[ " + down.toString() + " ]");
+    }
 
+    public void handlePrintMap(String direction, boolean isCorrect) {
+        if (up.toString().isEmpty() && down.toString().isEmpty()) {
+            fillInMapAtFirst(direction, isCorrect);
+            return;
+        }
+        fillInMapAtAfter(direction, isCorrect);
+    }
+
+    public void fillInMapAtFirst(String direction, boolean isCorrect) {
+        if (direction.equals("U")) {
+            up.append(getMark(isCorrect));
+            down.append(" ");
+        }
+        if (direction.equals("D")) {
+            up.append(" ");
+            down.append(getMark(isCorrect));
+        }
+    }
+
+    public void fillInMapAtAfter(String direction, boolean isCorrect) {
+        if (direction.equals("U")) {
+            up.append(" | " + getMark(isCorrect));
+            down.append(" |  ");
+        }
+        if (direction.equals("D")) {
+            up.append(" |  ");
+            down.append(" | " + getMark(isCorrect));
+        }
+    }
+
+    public String getMark(boolean isCorrect) {
+        if (isCorrect) {
+            return "O";
+        }
+        return "X";
     }
 
     /**
@@ -24,9 +63,6 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
-    }
-
     public void printResult(BridgeGame bridgeGame) {
         System.out.println("\n게임 성공 여부: " + isWin(bridgeGame.isGameWin()));
         System.out.println("총 시도한 횟수: " + bridgeGame.getTryCount());
