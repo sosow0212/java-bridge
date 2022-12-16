45cb317de533b7fde458fb3928294bda42047067
sosow0212 <sosow0212@naver.com>
2022-12-16 16:09:37 +0900

    feat : (BridgeRandomNumberGeneratorTest) 브릿지 게임 테스트를 위한 테스트용 인터페이스 상속 클래스 생성

---
+++ src/test/java/bridge/bridgeMaker/BridgeRandomNumberGeneratorTest.java
@@ -0,0 +1,10 @@
+package bridge.bridgeMaker;
+
+import bridge.BridgeNumberGenerator;
+
+public class BridgeRandomNumberGeneratorTest implements BridgeNumberGenerator {
+    @Override
+    public int generate() {
+        return 1;
+    }
+}
