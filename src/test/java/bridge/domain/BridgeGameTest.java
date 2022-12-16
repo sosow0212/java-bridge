e3002f67f5220bc4defd1a37a3436575f15290ac
sosow0212 <sosow0212@naver.com>
2022-12-16 17:56:31 +0900

    refactor : (BridgeGameTest) 안쓰는 메서드 제거 후 테스트 리팩토링

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -10,15 +10,31 @@ public class BridgeGameTest {
     private final List<String> testBridge = List.of("U", "U", "U");
 
     @Test
-    @DisplayName("이동 실패 테스트")
-    public void moveFailTest() {
+    @DisplayName("유저의 정답이 맞는지 확인")
+    public void isUserAnswerCorrectTest() {
+        // given
+        BridgeGame bridgeGame = new BridgeGame(testBridge);
+        String direction = "U";
+
+        // when
+        boolean result = bridgeGame.isUserAnswerCorrect(direction);
+
+        // then
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("게임 성공 테스트")
+    public void gameWinTest() {
         // given
         BridgeGame bridgeGame = new BridgeGame(testBridge);
 
         // when
-        bridgeGame.moveFail();
+        bridgeGame.move();
+        bridgeGame.move();
+        bridgeGame.move();
 
         // then
-        assertThat(bridgeGame.isGameDone()).isEqualTo(true);
+        assertThat(bridgeGame.isGameWin()).isEqualTo(true);
     }
 }
