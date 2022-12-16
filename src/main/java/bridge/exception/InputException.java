277ca09393ea6e2923d42f984d6fb4230a0d2320
sosow0212 <sosow0212@naver.com>
2022-12-16 18:08:19 +0900

    feat : (InputException) 방향과, 게임 상태 예외 추가

--- src/main/java/bridge/exception/InputException.java
+++ src/main/java/bridge/exception/InputException.java
@@ -5,8 +5,11 @@ import java.util.List;
 public class InputException {
     private final static int MIN_SIZE = 3;
     private final static int MAX_SIZE = 20;
-    private final static List<String> INPUT_CASE = List.of("U", "D");
+    private final static List<String> VALID_DIRECTION_CASE = List.of("U", "D");
+    private final static List<String> VALID_COMMAND_CASE = List.of("R", "Q");
     private final static String ONLY_CONTAINS_NUMBER_MESSAGE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    private final static String NOT_VALID_DIRECTION_MESSAGE = "방향은 U, D 입력만 가능합니다.";
+    private final static String NOT_VALID_COMMAND_MESSAGE = "입력은 R, Q만 가능합니다.";
 
     public void validateBridgeSize(String input) {
         checkBridgeSizeOnlyContainsNumbers(input);
@@ -14,7 +17,23 @@ public class InputException {
     }
 
     public void validateDirection(String input) {
+        checkIsCorrectValueOfDirection(input);
+    }
 
+    public void validateCommand(String input) {
+        checkIsCorrectValueOfCommand(input);
+    }
+
+    private void checkIsCorrectValueOfCommand(String input) {
+        if(!VALID_COMMAND_CASE.contains(input)) {
+            throw new IllegalArgumentException(NOT_VALID_COMMAND_MESSAGE);
+        }
+    }
+
+    private void checkIsCorrectValueOfDirection(String input) {
+        if(!VALID_DIRECTION_CASE.contains(input)) {
+            throw new IllegalArgumentException(NOT_VALID_DIRECTION_MESSAGE);
+        }
     }
 
     private void checkBridgeSizeOnlyContainsNumbers(String input) {
@@ -27,7 +46,7 @@ public class InputException {
 
     private void checkIsBridgeSizeValidLength(String input) {
         int size = Integer.valueOf(input);
-        if (!(size >= MIN_SIZE && size <= 20)) {
+        if (!(size >= MIN_SIZE && size <= MAX_SIZE)) {
             throw new IllegalArgumentException(ONLY_CONTAINS_NUMBER_MESSAGE);
         }
     }
