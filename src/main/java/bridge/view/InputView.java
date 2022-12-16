ffffdf68a7ccc3d44c3163d6cb8a5049ea76d7f1
sosow0212 <sosow0212@naver.com>
2022-12-16 16:19:40 +0900

    feat : (InputView) 기본적인 게임의 입력 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,55 @@
 package bridge.view;
 
+import static bridge.view.OutputView.printError;
+
+import bridge.util.InputParser;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final InputParser inputParser = new InputParser();
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        try {
+            String input = Console.readLine();
+            // 예외
+            return inputParser.parsingBridgeSize(input);
+        } catch (IllegalArgumentException exception) {
+            printError(exception.getMessage());
+            return readBridgeSize();
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        try {
+            String input = Console.readLine();
+            // 예외
+            return input;
+        } catch (IllegalArgumentException exception) {
+            printError(exception.getMessage());
+            return readMoving();
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        try {
+            String input = Console.readLine();
+            // 예외
+            return input;
+        } catch (IllegalArgumentException exception) {
+            printError(exception.getMessage());
+            return readGameCommand();
+        }
     }
 }
